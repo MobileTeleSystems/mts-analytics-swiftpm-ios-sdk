b88c982d688860eda0412ffb074b5f3776e5d6c1
MTSAnalyticsBot <168260334+MTSAnalyticsBot@users.noreply.github.com>
2024-06-18 16:16:19 +0000

    Release version 2.2.1

--- Package.swift
+++ Package.swift
@@ -6,20 +6,21 @@ import PackageDescription
 let package = Package(
     name: "MTSAnalytics",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .tvOS(.v13)
     ],
     products: [
          .library(name: "MTSAnalytics", targets: ["MTSAnalytics"])
     ],
     dependencies: [
       	.package(url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.11.0")),
-	.package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.22.1"))
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.26.0"))
     ],
     targets: [
         .binaryTarget(
             name: "MTSAnalytics",
-            url: "https://github.com/MobileTeleSystems/mts-analytics-swiftpm-ios-sdk/releases/download/2.1.0/MTSAnalytics-2.1.0.zip",
-            checksum: "7a5275d775d0ebf5419429e117cd07d1c5ff608385887a75eb0cc14d41092de2"
+            url: "https://github.com/MobileTeleSystems/mts-analytics-swiftpm-ios-sdk/releases/download/2.2.1/MTSAnalytics-2.2.1.zip",
+            checksum: "44ef122c76c110fa28ce192259e961225981977a1779cdea178b58330edf346d"
          )
     ]
 )
