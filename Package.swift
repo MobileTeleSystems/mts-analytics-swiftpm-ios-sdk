5d074c9042b648769188fbdd8980cc462ac0f397
MTSAnalyticsBot <168260334+MTSAnalyticsBot@users.noreply.github.com>
2024-06-18 16:14:39 +0000

    Release version 1.1.2

--- Package.swift
+++ Package.swift
@@ -12,16 +12,22 @@ let package = Package(
          .library(name: "MTSAnalytics", targets: ["MTSAnalytics"])
     ],
     dependencies: [
-        .package(url: "https://github.com/stephencelis/SQLite.swift", .upToNextMajor(from: "0.14.1")),
-        .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios", .upToNextMajor(from: "1.3.0")),
-      	.package(url: "https://github.com/microsoft/plcrashreporter", .upToNextMajor(from: "1.11.0")),
-	.package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.22.1")),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.14.1")),
+        .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios.git", .upToNextMajor(from: "1.3.0")),
+      	.package(url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMajor(from: "1.11.0")),
+	.package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.22.1"))
     ],
     targets: [
         .binaryTarget(
             name: "MTSAnalytics",
-            url: "https://github.com/MobileTeleSystems/mts-analytics-swiftpm-ios-sdk/releases/download/1.1.1/MTSAnalytics-1.1.1.zip",
-            checksum: "c78c35ea496396b976518840df5bad2c62f8632a91aad9bc6bf0fb6d272af03e"
+            url: "https://github.com/MobileTeleSystems/mts-analytics-swiftpm-ios-sdk/releases/download/1.1.2/MTSAnalytics-1.1.2.zip",
+            checksum: "8c33d07e09dafbae0c9c6b6645d4195b65c4baa64d2e5286f5fe261b8519e767",
+	    dependencies: [
+		"SQLite",
+		"FingerprintJS",
+		"PLCrashReporter",
+		"SwiftProtobuf"
+	    ]
          )
     ]
 )
